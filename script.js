be5569211307df41230d6bc4deb6204950a14dde
Jatin <jatinsulediya02@gmail.com>
2026-09-18 17:26:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var btc = document.getElementById("bitcoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
+
+
+var settings = {
+    "async" : true,
+    "scrossDomain" :true,
+    "url" : "https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=bitcoin,ethereum,dogecoin",
+    "method" : "GET",
+    "headers" : {}
+
+}
+$.ajax(settings).done(function (response){
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+})
